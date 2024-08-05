5415c00dee48f9d5538c9ca0f2c12ea41635c280
Ludwig <ludwig@webdeclic.com>
2024-08-05 12:32:40 +0100

    Add cache on shortcode get acf subfield for better performances

--- acf/shortcode-get-acf-subfield/functions.php
+++ acf/shortcode-get-acf-subfield/functions.php
@@ -12,14 +12,29 @@ add_shortcode( 'get_acf_subfield', 'shortcode_get_acf_subfield' );
  * @return mixed
  */
 function shortcode_get_acf_subfield( $atts ) {
+	global $shortcode_get_acf_subfield_cache;
+	
+	if( !is_array($shortcode_get_acf_subfield_cache) ){
+		$shortcode_get_acf_subfield_cache = array();
+	}
+
 	$a = shortcode_atts( array(
 		'post_id' => '',
 		'field'   => '',
 		'path'    => '',
+		'cache'   => 'true',
 	), $atts );
   
 	$post_id = !empty($a['post_id']) ? $a['post_id'] : get_the_ID();
-	$data    = get_field( $a['field'], $post_id );
+	
+	if( $a['cache'] == 'true' && isset($shortcode_get_acf_subfield_cache[$post_id][$a['field']]) ){
+		$data = $shortcode_get_acf_subfield_cache[$post_id][$a['field']];
+	} else {
+		$data = get_field( $a['field'], $post_id );
+		if( $a['cache'] == 'true' ){
+			$shortcode_get_acf_subfield_cache[$post_id][$a['field']] = $data;
+		}
+	}
 	
 	if( !empty($a['path']) ){
 	  $path = explode('->', $a['path']);
